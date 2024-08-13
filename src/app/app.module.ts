e13af00e60afe8f7e0df2727e7f13a1573ed14a3
Harpinder Sandhu <harpindersandhu18@Gmail.com>
2024-08-12 18:17:19 -0600

    Initial_commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskComponent } from './components/task/task.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TasksComponent,
+    TaskComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
